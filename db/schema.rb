3d1431a359b5b319af9e03cfb38c6e7ca8bf7f1c
Werliton <werliton.carlos@ceuma.br>
2018-04-12 11:47:59 -0300

    Usando método de validação

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412134310) do
+ActiveRecord::Schema.define(version: 20180412142212) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
     t.string "location"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "password"
+    t.string "location"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
