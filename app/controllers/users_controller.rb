c5fef0ce9f20bdd2bbbb9907a8faa5a31646d9be
Werliton <werliton.carlos@ceuma.br>
2018-04-12 17:03:06 -0300

    Criando model user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def index
+    @teste = 'Ruby poderoso para criacao de sistemas web'
+  end
+
+  def show
+    @user = User.find(user_params)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user,
+                  :notice => 'Cadastro criado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:full_name, :location, :description, :description)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
-  validates_presence_of :email, :full_name, :location, :password
+  validates_presence_of :email, :full_name, :location, :bio#, :password
   validates_confirmation_of :password
-  validates_length_of :bio, :minimum => 30, :allow_blank => false
-  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\Z/
-  validates_uniqueness_of :email
+  validates_length_of :bio, :minimum => 20, :allow_blank => false, :message => 'Informe no mínimo 20 caracteres'
+  #validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  #validates_uniqueness_of :email
 end
