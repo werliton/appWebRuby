146927e8d94b443d987e29ca7b7796f7cef9cbf8
Werliton <werliton.carlos@ceuma.br>
2018-04-16 11:15:41 -0300

    Exibindo erros na tela de cadastrio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  #validates_presence_of :email, :full_name, :location, :bio#, :password
+  validates_presence_of :email, :full_name, :location, :bio#, :password
   #validates_confirmation_of :password
   #validates_length_of :bio, :minimum => 20, :allow_blank => false, :message => 'Informe no mínimo 20 caracteres'
   #validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
