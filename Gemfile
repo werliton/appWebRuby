15ece5cffe23c64a8c85fd73f764464ad9b4b364
Werliton <werliton.carlos@ceuma.br>
2018-04-20 09:42:29 -0300

    Criando modelo de securança de senha

--- Gemfile
+++ Gemfile
@@ -29,7 +29,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-gem 'bcrypt-ruby', '~> 3.1', '>= 3.1.5'
+# gem 'bcrypt-ruby', '~> 3.1', '>= 3.1.5'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
   validates_presence_of :full_name, :location, :email
-    #, :bio#, :password
-  #validates_confirmation_of :password
   #validates_length_of :bio, :minimum => 20, :allow_blank => false, :message => 'Informe no mínimo 20 caracteres'
-  #validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  #validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates_uniqueness_of :email
+
+  has_secure_password
 end
