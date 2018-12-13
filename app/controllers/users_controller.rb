37103d219a5a7254553fb16f7ec265078b93634e
werliton <werlitoncarlos@gmail.com>
2018-12-13 17:21:36 -0300

    Criando tela home e seus recursos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user.destroy
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
