6a313ca809f4cbf816a91bb3e713cfdfcf39d465
werliton <werlitoncarlos@gmail.com>
2018-12-14 14:51:01 -0300

    Criando templates de permissões

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -33,4 +33,12 @@ class UserSession
     def persisted?
         false
     end
+
+    def current_user
+        User.find(@session[:user_id])
+    end
+
+    def user_signed_in?
+        @session[:user_id].present?
+    end
 end
