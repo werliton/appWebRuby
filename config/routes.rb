a9aeaee5d11570d3445cb64f719edf9b7c567733
werliton <werlitoncarlos@gmail.com>
2018-12-14 10:26:59 -0300

    Finalizando suporte a mais de um idioma

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+  LOCALES = /en|pt\-BR/
+  scope "(:locale)", :locale => LOCALES do
+    resources :rooms
+    resources :users
+
+    resource :user_confirmation, :olny => [:show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/:locale' => 'home#index', :locale => LOCALES, :via => [:get, :post]
   root :to => "home#index"
 end
