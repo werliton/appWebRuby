57fac7fbeaa93edb51b0042653b850d1ec23f0aa
werliton <werlitoncarlos@gmail.com>
2018-12-15 11:05:06 -0300

    Criando presenter room

---
+++ app/presenters/room_presenter.rb
@@ -0,0 +1,38 @@
+class RoomPresenter
+    delegate :user, :created_at, :description, :location, :title, :to => :@room
+
+    def initialize(rooms, context, show_form=true)
+        @context = context
+        @room = room
+        @show_form = show_form
+    end
+
+    def can_review?
+        @context.user_signed_in?
+    end
+
+    def show_form?
+        @show_form
+    end
+
+    def review
+        @review ||= @room.reviews.
+            find_or_initialize_by_user_id(@context.current_user.id)
+    end
+
+    def review_route
+        [@room, review]
+    end
+
+    def route
+        @room
+    end
+
+    def review_points
+        Review::POINTS
+    end
+
+    def to_partial_path
+        'room'
+    end
+end
